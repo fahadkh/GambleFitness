86e6db26a059901b7d51905607f255607431e7c8
Rovik Robert <rovikjeremiah@gmail.com>
2017-02-25 22:44:35 -0600

    Bug Fix
    set time transitions from within pages rather than splashscreen

--- mobile/src/main/java/io/github/fahadkh/gamblefitness/NightMode.java
+++ mobile/src/main/java/io/github/fahadkh/gamblefitness/NightMode.java
@@ -1,7 +1,11 @@
 package io.github.fahadkh.gamblefitness;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Calendar;
 
 public class NightMode extends AppCompatActivity {
 
@@ -9,6 +13,17 @@ public class NightMode extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_night_mode);
+
+        int currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY); //Current hour
+        if (currentHour > 4 && currentHour < 22){
+            this.gotoDaily();
+        }
+    }
+
+    public void gotoDaily() {
+        Intent intent;
+        intent = new Intent(this, DailyGoal.class);
+        startActivity(intent);
     }
 
     public void onBackPressed()
